26447be5fd70967a77be8e44394249ddbdb1d1f6
dingy007 <dingy007@gmail.com>
2015-03-23 12:15:56 +0800

    Changing directory structure to support Maven

---
+++ src/main/java/com/koreinfo/crm/streakapi/queryStreak/IStreakAPI.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package main.com.koreinfo.crm.streakapi.queryStreak;
+
+import main.com.koreinfo.crm.streakapi.queryStreak.resources.streakObjects.User;
+
+/**
+ * @author Administrator
+ *
+ */
+public interface IStreakAPI {
+	public User getCurrentUser();
+	
+}
